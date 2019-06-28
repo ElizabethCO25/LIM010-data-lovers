09d666fded3cdf19b2d58e30d12d1435417ec825
ElizabethCO25 <maria250593@gmail.com>
2019-06-28 12:46:36 -0500

    Implementando menú desplegable - sin estilo

--- src/main.js
+++ src/main.js
@@ -2,21 +2,35 @@
 //Login:
 const obtenerClick = document.getElementById("boton-ingresar");
 const capturarContraseña = document.getElementById("input-contraseña");
+const capturarUsuario = document.getElementById("usuario")
 let cuenta = 2;
 obtenerClick.addEventListener("click", () => {
-  if (capturarContraseña.value === "LABORATORIA") {
-    alert("Contraseña Correcta");
+  if (capturarContraseña.value === "LABORATORIA" && capturarUsuario.value === "LABORATORIA") {
+    alert("Contraseña y usuario correcto!");
     document.getElementById("pantalla-inicio").classList.remove("ocultar");
     document.getElementById("pantalla-login").classList.add("ocultar");
     capturarContraseña.value = "";
-  } else if (cuenta === 0) {
+    capturarUsuario.value = "";
+  } else if (capturarContraseña.value === "LABORATORIA" && capturarUsuario.value != "LABORATORIA"){
+    alert("Usuario incorrecto");
+    cuenta--;
+    capturarContraseña.value = "";
+    capturarUsuario.value = "";
+  } else if (capturarContraseña.value != "LABORATORIA" && capturarUsuario.value != "LABORATORIA"){
+    alert("Contraseña y usuario incorrectos");
+    cuenta--;
+    capturarContraseña.value = "";
+    capturarUsuario.value = "";
+  }else if (cuenta === 0) {
     alert("Agotaste tus intentos de ingreso");
     document.getElementById("pantalla-bloqueo").classList.remove("ocultar");
     document.getElementById("pantalla-login").classList.add("ocultar");
     capturarContraseña.value = "";
-  } else {
-    alert("Vuelve a intentarlo");
+    capturarUsuario.value = "";
+  }else {
+    alert("Contraseña incorrecta");
     capturarContraseña.value = "";
+    capturarUsuario.value = "";
     cuenta--;
   }
 });
