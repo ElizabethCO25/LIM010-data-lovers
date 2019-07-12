a50e197d012933f7606022a76373ab6403176a66
ElizabethCO25 <maria250593@gmail.com>
2019-07-12 09:11:51 -0500

    Editando media querys

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-/* Manejo del DOM */
-//Declarando variables
-const obtenerClick = document.getElementById("boton-ingresar");
-const capturarContraseña = document.getElementById("input-contraseña");
-const capturarUsuario = document.getElementById("input-usuario");
-const allPokemon = POKEMON.pokemon;
-let cuenta = 2;
-obtenerClick.addEventListener("click", () => {
-  if (capturarContraseña.value === "1" && capturarUsuario.value === "1") {
-    document.getElementById("pantalla-inicio").classList.remove("ocultar");
-    document.getElementById("header").classList.remove("ocultar");
-    document.getElementById("pantalla-login").classList.add("ocultar");
-    capturarContraseña.value = "";
-    capturarUsuario.value = "";
-  } else if (cuenta === 0) {
-    document.getElementById("pantalla-bloqueo").classList.remove("ocultar");
-    document.getElementById("pantalla-login").classList.add("ocultar");
-    capturarContraseña.value = "";
-  } else {
-    document.getElementById("incorrecto").innerHTML = "Contraseña o usuario incorrecto,intenta otra vez";
-    capturarContraseña.value = "";
-    cuenta--;
-  }
-});
+  /* Manejo del DOM */
+  //Declarando variables
+  const obtenerClick = document.getElementById("boton-ingresar");
+  const capturarContraseña = document.getElementById("input-contraseña");
+  const capturarUsuario = document.getElementById("input-usuario");
+  const allPokemon = POKEMON.pokemon;
+  let cuenta = 2;
+  obtenerClick.addEventListener("click", () => {
+    if (capturarContraseña.value === "1" && capturarUsuario.value === "1") {
+      document.getElementById("pantalla-inicio").classList.remove("ocultar");
+      document.getElementById("header").classList.remove("ocultar");
+      document.getElementById("pantalla-login").classList.add("ocultar");
+      capturarContraseña.value = "";
+      capturarUsuario.value = "";
+    } else if (cuenta === 0) {
+      document.getElementById("pantalla-bloqueo").classList.remove("ocultar");
+      document.getElementById("pantalla-login").classList.add("ocultar");
+      capturarContraseña.value = "";
+    } else {
+      document.getElementById("incorrecto").innerHTML = "Contraseña o usuario incorrecto,intenta otra vez";
+      capturarContraseña.value = "";
+      cuenta--;
+    }
+  });
 
 //Función ENTER:
 let enterIniciar = document.getElementById("input-contraseña");
